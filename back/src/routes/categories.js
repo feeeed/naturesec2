6257e6875267e04c1154c74382997325c1fb23e6
feeeed <76104017+feeeed@users.noreply.github.com>
2023-09-12 02:23:53 +0600

    new ver
    asd

--- back/src/routes/categories.js
+++ back/src/routes/categories.js
@@ -4,10 +4,10 @@ const checkJWTSign = require('../middlewares/jwtCheck.middleware')
 const {category} = require('../controllers')
 
 router.route('/:id').get(category.get)
-router.route('/').post(category.create)
+//router.route('/').post(category.create)
 router.route('/').get(category.getAll)
-router.route('/:id').put(category.update)
-router.route('/:id').delete(category.delete)
+//router.route('/:id').put(category.update)
+//router.route('/:id').delete(category.delete)
 
 
 module.exports = router
--- back/src/routes/posts.js
+++ back/src/routes/posts.js
@@ -3,10 +3,10 @@ const router = require('express-promise-router')()
 const {post} = require('../controllers')
 
 router.route('/:id').get(post.get)
-router.route('/').post(post.create)
+//router.route('/').post(post.create)
 router.route('/').get(post.getAll)
-router.route('/:id').get(post.update)
-router.route('/:id').get(post.delete)
+//router.route('/:id').get(post.update)
+//router.route('/:id').get(post.delete)
 
 
 module.exports = router
